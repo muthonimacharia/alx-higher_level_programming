b82255e928b5a2f4fd55ffed0c5df85dcd78c9f7
muthonimacharia <paullymuthoni@gmail.com>
2022-05-31 22:50:37 +0300

    random

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,11 @@
+#!usr/bin/python
+import random
+number = random.randint(-10,10)
+last_num = abs(number) % 10
+
+if number < 0;
+    last_num = -last_num
+    print(number, last_num)
+else:
+    last_num = last_num
+    print(number, last_num)
