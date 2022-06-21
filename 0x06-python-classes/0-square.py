1eb7b43b7248033f2b09d2051a10f98aaf22ab84
muthonimacharia <paullymuthoni@gmail.com>
2022-06-21 23:24:34 +0300

    py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" this module defines a empty class square """
+
+
 class Square:
-    """Type class creation of empty class square"""
+    """ empty class for task 0 """
     pass
